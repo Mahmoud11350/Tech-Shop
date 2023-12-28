1fc28b0b2d583487b464f93d8e3b267d89675342
mahmoud11350 <mahmoudsalah11350@gmail.com>
2023-12-28 07:06:26 +0200

    fix navbar

--- components/navbar/MobileNavbar.jsx
+++ components/navbar/MobileNavbar.jsx
@@ -7,24 +7,26 @@ import { useState } from "react";
 const MobileNavbar = () => {
   const [visible, setVisible] = useState(true);
   return (
-    <div className="md:hidden relative overflow-hidden">
+    <div className="md:hidden  ">
       <Image src={menuIcon} onClick={() => setVisible(!visible)} />
 
       <ul
-        className={`flex flex-col absolute  z-10 bg-accent w-screen h-40 right-0`}
+        className={`flex flex-col absolute top-24 w-screen left-0  z-20 bg-accent/25  h-40 items-center justify-center transition-[all_1s] ${
+          visible ? "left-0" : "-left-[100vw]"
+        }`}
       >
         <li>
-          <Link href={"/"} className="btn btn-ghost text-md">
+          <Link href={"/"} className="btn btn-ghost text-md capitalize">
             home
           </Link>
         </li>
         <li>
-          <Link href={"/products"} className="btn btn-ghost text-md">
+          <Link href={"/products"} className="btn btn-ghost text-md capitalize">
             shop{" "}
           </Link>
         </li>
         <li>
-          <Link href={"/"} className="btn btn-ghost text-md">
+          <Link href={"/"} className="btn btn-primary text-md capitalize">
             login
           </Link>
         </li>
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -4,23 +4,25 @@ import MobileNavbar from "./MobileNavbar";
 
 const Navbar = () => {
   return (
-    <nav className="container flex h-[100px] justify-between items-center">
-      <div className="uppercase font-bold text-2xl flex items-center gap-2">
-        <IoBagHandleSharp className="" />
-        tech shop
+    <nav className="relative">
+      <div className="container flex h-[100px] justify-between items-center">
+        <div className="uppercase font-bold text-2xl flex items-center gap-2">
+          <IoBagHandleSharp className="" />
+          tech shop
+        </div>
+        <MobileNavbar />
+        <ul className=" gap-4 capitalize hidden md:flex">
+          <li className="btn btn-ghost text-md">
+            <Link href={"/"}>home</Link>
+          </li>
+          <li className="btn btn-ghost text-md">
+            <Link href={"/products"}>shop </Link>
+          </li>
+          <li className="btn btn-primary text-md text-white">
+            <Link href={"/"}>login</Link>
+          </li>
+        </ul>
       </div>
-      <MobileNavbar />
-      <ul className=" gap-4 capitalize hidden md:flex">
-        <li className="btn btn-ghost text-md">
-          <Link href={"/"}>home</Link>
-        </li>
-        <li className="btn btn-ghost text-md">
-          <Link href={"/products"}>shop </Link>
-        </li>
-        <li className="btn btn-primary text-md text-white">
-          <Link href={"/"}>login</Link>
-        </li>
-      </ul>
     </nav>
   );
 };
